dc9f174cb2292712053429ebf5b8b81550a95cd6
Lucas <melibauducco@gmail.com>
2025-12-23 11:10:33 -0300

    Fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // IMPORTANTE: Cambia 'dashboard-financiero' por el nombre de tu repositorio en GitHub
-  base: '/dashboard-financiero/',
+  base: '/dashboardfinancierosistemas/',
 })
+
